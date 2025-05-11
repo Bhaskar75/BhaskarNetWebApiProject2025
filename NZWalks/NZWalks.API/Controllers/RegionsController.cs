95329ec9a4fc1796c3286e28e8e4d12733b694c8
Bhaskar Manna <34345805+Bhaskar75@users.noreply.github.com>
2025-05-11 14:04:13 +0530

    Completed upto delete code

--- NZWalks/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -46,10 +46,10 @@ namespace NZWalks.API.Controllers
             {
                 regionsDTO.Add(new RegionDTO()
                 {
-                    Id= region.Id,
-                    Code= region.Code,
-                    Name= region.Name,
-                    RegionImageUrl= region.RegionImageUrl,
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl,
                 });
             }
 
@@ -81,5 +81,95 @@ namespace NZWalks.API.Controllers
             return Ok(regionsDTO);
         }
 
+        //POST To create New Region
+        //POST: https://localhost:portnumber/api/regions
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDTO addRegionRequestDTO)
+        {
+            //Map or convert DTO to Domain model
+            var regionDomainModel = new Region
+            {
+                Code = addRegionRequestDTO.Code,
+                Name = addRegionRequestDTO.Name,
+                RegionImageUrl = addRegionRequestDTO.RegionImageUrl
+            };
+
+            //Use Domain Model to Create Region
+            nZWalksDbContext.Regions.Add(regionDomainModel);
+            nZWalksDbContext.SaveChanges();
+
+            //map domain model to DTO
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+
+        //Update region
+        //PUT: https://localhost:portnumber/api/regions/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDTO updateRegionRequestDTO)
+        {
+            //Check if region exists
+            var regionDomainModel = nZWalksDbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //Map DTO to Domain model
+            regionDomainModel.Code = updateRegionRequestDTO.Code;
+            regionDomainModel.Name = updateRegionRequestDTO.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDTO.RegionImageUrl;
+
+            nZWalksDbContext.SaveChanges();
+
+            //Convert Domain Model To DTO
+            var RegionDTO = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+
+            return Ok(RegionDTO);
+        }
+
+        //Delete Region
+        //DELETE: https://localhost:portnumber/api/regions/{id}
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var regionDomainModel = nZWalksDbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomainModel == null)
+            {
+                return NotFound();
+            }
+
+            //Delete region
+            nZWalksDbContext.Regions.Remove(regionDomainModel);
+            nZWalksDbContext.SaveChanges();
+
+            //return Deleted Region Back
+            //map domain model to DTO
+
+            var RegionDTO = new RegionDTO
+            {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return Ok(RegionDTO);
+
+        }
     }
 }
